fab95cff93264f54c7aba9a34b02a2fa48bb7179
Andres Soruco <mam20002@byui.edu>
2023-09-25 16:46:11 -0400

    js file sintax fixed

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -10,13 +10,13 @@ const profilePicture = "images/soruco2-copia.jpg";
 
 /*Step 3 - Element Variables */
 
-const nameElement = docuemnt.getElementById("name");
+const nameElement = document.getElementById("name");
 const foodElement = document.getElementById("food");
 const yearElement = document.querySelector("#year");
 const imageElement = docuemnt.getElementsByTagName("img");
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = `<strong>${fullname}</strong>`;
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 nameElement.textContent = currentYear;
 imageElement.setAttribute("src", profilePicture);
 imageElement.setAttribute("alt", `Profile image of ${fullName}`);
